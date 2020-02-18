63648dd98f7ee11d56a08c1a5e3467995ad032b5
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-02-19 00:44:59 +0530

    main method make manudriven

--- src/implementation/Main.java
+++ src/implementation/Main.java
@@ -2,30 +2,45 @@ package implementation;
 
 import definition.QuequeList;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.printf("press 1 for add element in queque\n " +
+                "press 2 for remove element in queque\n " +
+                "press 3 for poll element in queque\n " +
+                "press 4 for element element in queque\n " +
+                "press 5 for peek element in queque\n" +
+                " press 6 for print queque ");
+        Scanner sc = new Scanner(System.in);
         QuequeList<String> list = new QuequeList<>();
-        //System.out.println(list);
-        //System.out.println(list.poll());
-        System.out.println(list.peek());
-        System.out.println(list.element());
-        list.add("ankit");
-        list.add("akash");
-        list.add("mahatma");
-        //list.add("gandhi", 3);
+        System.out.println("first Queque is empty " + list);
+        String ch = sc.nextLine();
+        switch (ch) {
+            case "1":
+                list.add(sc.nextLine());
+                System.out.println("element is added");
+                break;
+            case "2":
+                System.out.println("removed element of queque using remove() method is " + list.remove());
+                break;
+            case "3":
+                System.out.println("removed element of queque using poll() method is " + list.poll());
+                break;
+            case "4":
+                System.out.println("topmost of element of queque is by using element() method is  " + list.element());
+                break;
+            case "5":
+                System.out.println("topmost of element of queque is by using peek() method is  " + list.peek());
+                break;
+            case "6":
+                System.out.println("new list is  " + list);
+                break;
+            default:
+                System.exit(0);
+                break;
 
-        System.out.println(list);
-        System.out.println(list.poll());
-        System.out.println(list.element());
 
-
-        System.out.println(list);
-
-        //System.out.println(list.peek());
-        //System.out.println(list);
-
-        //System.out.println(list.poll());
-        //System.out.println(list);
-        //System.out.println(list);
+        }
     }
 }

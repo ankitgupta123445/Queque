910c418afe88df566a6e470303b127c9d31031c0
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-02-18 23:12:21 +0530

    documentation is added

--- src/adt/QuequeAdt.java
+++ src/adt/QuequeAdt.java
@@ -4,19 +4,31 @@ public interface QuequeAdt<E> {
     boolean add(E item);
 
     boolean add(E item, int index);
+
     /*
  The remove() and poll() methods remove and return the head of the queue. Exactly which element is removed from the queue is a function
 of the queue's ordering policy, which differs from implementation to implementation. The remove() and poll() methods differ only in their behavior
  when the queue is empty: the remove() method throws an exception, while the poll() method returns null.
 */
-
     E remove();
-
-    E peek();
 
     E poll();
 
+    /*
+E peek()
+Retrieves, but does not remove, the head of this queue, or returns null if this queue is empty.
+
+*/
+    E peek();
+
+
+
+    /* E element()
+    Retrieves, but does not remove, the head of this queue. This method differs from peek only in that it throws an exception if this queue is empty.
+*/
+
     E element();
 }
+
 
 

45bd773de0be4d6987ee9e95919d0984c95873f8
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-02-18 23:07:31 +0530

    add remove & poll documentation

--- src/adt/QuequeAdt.java
+++ src/adt/QuequeAdt.java
@@ -4,6 +4,11 @@ public interface QuequeAdt<E> {
     boolean add(E item);
 
     boolean add(E item, int index);
+    /*
+ The remove() and poll() methods remove and return the head of the queue. Exactly which element is removed from the queue is a function
+of the queue's ordering policy, which differs from implementation to implementation. The remove() and poll() methods differ only in their behavior
+ when the queue is empty: the remove() method throws an exception, while the poll() method returns null.
+*/
 
     E remove();
 
@@ -13,4 +18,5 @@ public interface QuequeAdt<E> {
 
     E element();
 }
+
 
